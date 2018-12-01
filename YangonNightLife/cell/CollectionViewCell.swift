72507bd75602248a8c22f8822cbc1f7a44da61b9
phyothinzaraung <phyothinzar.ag92@gmail.com>
2018-12-02 00:40:34 +0630

    initial commit

---
+++ YangonNightLife/cell/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  YangonNightLife
+//
+//  Created by Phyo Thinzar Aung on 12/2/18.
+//  Copyright © 2018 padc. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ YangonNightLife/controller/RestaurantViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantViewController.swift
+//  YangonNightLife
+//
+//  Created by Phyo Thinzar Aung on 12/1/18.
+//  Copyright © 2018 padc. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

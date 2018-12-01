bddba49056cd7e83d0a7cd0f8e45da2f7b870169
phyothinzaraung <phyothinzar.ag92@gmail.com>
2018-12-01 18:35:23 +0630

    Initial Commit

---
+++ YangonNightLife/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  YangonNightLife
+//
+//  Created by Phyo Thinzar Aung on 12/1/18.
+//  Copyright © 2018 padc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ YangonNightLife/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  YangonNightLife
+//
+//  Created by Phyo Thinzar Aung on 12/1/18.
+//  Copyright © 2018 padc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
